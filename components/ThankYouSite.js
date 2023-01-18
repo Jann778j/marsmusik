771fdeece0d97f73834d842b0e97a73a3eb63933
janne hjuler jensen <jann778j@stud.kea.dk>
2023-01-18 19:09:27 +0100

    hrfhr

--- components/ThankYouSite.js
+++ components/ThankYouSite.js
@@ -9,7 +9,6 @@ function ThankYouSite(props) {
       <div className="thank_container">
         <h1>Thank you!</h1>
         <h3>Booking has been sent</h3>
-        <Basket></Basket>
         <button>
           <Anchor href="/"> Back to Home</Anchor>
         </button>
